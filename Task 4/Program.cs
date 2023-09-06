fab9f8b749f4dc51647bb4e653cf9e5046c6e8b6
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-06 23:03:53 +0300

    Task4

---
+++ Task 4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все четные числа от 1 до N;
+
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= N; i += 2)
+  if (i % 2 == 0)
+    Console.WriteLine($"{i}");
+
