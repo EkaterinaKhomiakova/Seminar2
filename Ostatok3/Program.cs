0a920b9f69aa1f2ce25ecaf6042d1bb0c7337f17
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-10 00:41:44 +0300

    Ostatok3

---
+++ Ostatok3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход 2 числа
+// и проверяет, является ли одно число квадратом другого
+// 5, 25 -> да
+// 6, 9 -> нет
+Console.WriteLine("Введите число x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x * x == y || y * y == x)
+  Console.WriteLine("да");
+else
+  Console.WriteLine("нет");
