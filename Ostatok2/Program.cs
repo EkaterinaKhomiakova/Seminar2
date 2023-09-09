1e3f5bac04dac21b7176b2dd01a5df7c461aeed4
EkaterinaKhomiakova <ekhomiakov@yandex.ru>
2023-09-10 00:26:40 +0300

    Ostatok2

---
+++ Ostatok2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимаети на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23
+Console.WriteLine("Введите число : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 7 == 0 && n % 23 == 0)
+  Console.WriteLine("кратно");
+else
+  Console.WriteLine("некратно");
+
